55422172aa136e972976e43e80125f22f2cf06f8
Chris Elsworth <chris@cae.me.uk>
2020-05-08 12:23:31 +0100

    add a few missing DEVICE_FUNCTION constants

--- lib/lxp/packet/device_functions.rb
+++ lib/lxp/packet/device_functions.rb
@@ -7,6 +7,12 @@ class LXP
       READ_INPUT   = 4
       WRITE_SINGLE = 6
       WRITE_MULTI  = 16
+
+      # not handled yet
+      READ_HOLD_ERROR    = 131
+      READ_INPUT_ERROR   = 132
+      WRITE_SINGLE_ERROR = 134
+      WRITE_MULTI_ERROR  = 144
     end
   end
 end
